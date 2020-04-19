9adc4a9b9555e72d4a0884da3640e8a7fbbf1601
Santiago Lopez Del Pino <santisoccer10@gmail.com>
2020-04-18 20:47:57 -0400

    updates

---
+++ script.js
@@ -0,0 +1,8 @@
+function addInfo() {
+    let name = document.getElementById("name").value;
+    let row = document.getElementById("row").value;
+    let issue = document.getElementById("issue").value;
+    
+    sheetAdd(client);
+}
+
